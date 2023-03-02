9fcf08c62174ca7b02b4eb779e92637e105bb209
AlexMav <106380799+AlexMavlyutov@users.noreply.github.com>
2023-03-02 16:25:53 +0300

    Update CoinTrigger.cs

--- CoinTrigger.cs
+++ CoinTrigger.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinTrigger : MonoBehaviour
 {
+    public static event UnityAction increaseOfCoins;
     private float _waitingTime = 10;
-    
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Player player))
         {
-            CoinCountWriter.coin++;
-            
+            increaseOfCoins?.Invoke();
+
             gameObject.SetActive(false);
 
             Invoke(nameof(ActivateCoin), _waitingTime);
