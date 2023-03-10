07e0b6d54fbaee988d05d9350038a1daa6711475
AlexMav <106380799+AlexMavlyutov@users.noreply.github.com>
2023-03-10 17:06:59 +0300

    Update CharacterMovement.cs

--- CharacterMovement.cs
+++ CharacterMovement.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private float MinGroundNormalY = .65f;
