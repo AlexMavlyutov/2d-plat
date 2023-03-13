9b7bc1e0dc9c47d4ea0772fa6cb3851ee0e02065
AlexMav <106380799+AlexMavlyutov@users.noreply.github.com>
2023-03-13 20:28:19 +0300

    Update and rename DisplayCoin.cs to CoinDisplayer.cs

--- CoinDisplayer.cs
+++ CoinDisplayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DisplayCoin : MonoBehaviour
+public class CoinDisplayer : MonoBehaviour
 {
     [SerializeField]  private Text coinText;
     [SerializeField] private CoinCollectionChecker[]  _coinTrigger ;
